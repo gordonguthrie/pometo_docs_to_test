02c657d8c25b29473c03ad700381f5d2742ab9df
Gordon Guthrie <gordon@hypernumbers.com>
2020-05-14 12:19:19 +0100

    write tests to the disk (redux, redux)

--- src/pometo_docs_to_tests_prv.erl
+++ src/pometo_docs_to_tests_prv.erl
@@ -48,18 +48,18 @@ format_error(Reason) ->
 
 make_tests(App) ->
     Root = rebar_app_info:dir(App),
-    GeneratedTestDir = Root ++ "/test/generated_tests",
-    ok = file:del_dir_r(GeneratedTestDir),
+    GeneratedTestDir = filename:join([Root, "test", "generated_tests"]),
+    ok = del_dir(GeneratedTestDir),
     ok = file:make_dir(GeneratedTestDir),
-    DocsFiles = get_files(Root ++ "/docs/*"),
+    DocsFiles = get_files(filename:join([Root, "docs", "*"])),
     [generate_tests(X, GeneratedTestDir) || X <- DocsFiles],
     ok.
 
 get_files(Root) ->
     RawFiles = filelib:wildcard(Root),
-    Files     = [{X}            || X <- RawFiles, filename:extension(X) == ".md"],
-    Dirs      = [X ++ "/*"      || X <- RawFiles, filelib:is_dir(X)],
-    DeepFiles = [get_files(X)   || X <- Dirs],
+    Files     = [{X}                   || X <- RawFiles, filename:extension(X) == ".md"],
+    Dirs      = [filename:join(X, "*") || X <- RawFiles, filelib:is_dir(X)],
+    DeepFiles = [get_files(X)          || X <- Dirs],
     [X || {X} <- Files ++ lists:flatten(DeepFiles)].
 
 generate_tests([], _GeneratedTestDir) -> ok;
@@ -142,6 +142,29 @@ read_l2(Id, Acc) ->
         {error, Err} -> {error, Err};
         eof          -> {ok, lists:reverse(Acc)}
     end.
+
+del_dir(Dir) ->
+   lists:foreach(fun(D) ->
+                    ok = file:del_dir(D)
+                 end, del_all_files([Dir], [])).
+
+del_all_files([], EmptyDirs) ->
+   EmptyDirs;
+del_all_files([Dir | T], EmptyDirs) ->
+   {ok, FilesInDir} = file:list_dir(Dir),
+   {Files, Dirs} = lists:foldl(fun(F, {Fs, Ds}) ->
+                                  Path = filename:join([Dir, F]),
+                                  case filelib:is_dir(Path) of
+                                     true ->
+                                          {Fs, [Path | Ds]};
+                                     false ->
+                                          {[Path | Fs], Ds}
+                                  end
+                               end, {[],[]}, FilesInDir),
+   lists:foreach(fun(F) ->
+                         ok = file:delete(F)
+                 end, Files),
+   del_all_files(T ++ Dirs, [Dir | EmptyDirs]).
 
 make_runner() ->
 "%\n" ++
