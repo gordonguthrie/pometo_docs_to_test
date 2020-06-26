314130b407d4dd72ce4e9b3f2ccf90790a06e630
Gordon Guthrie <gordon@hypernumbers.com>
2020-06-26 11:09:47 +0200

    try and add lazy results (debuggin VII)

--- src/pometo_docs_to_tests_prv.erl
+++ src/pometo_docs_to_tests_prv.erl
@@ -134,23 +134,23 @@ gen_test3(["```pometo" ++ _Rest | T], ?IN_TEXT, Test, Acc) ->
         gen_test3(T, ?GETTING_TEST, #test{seq = N + 1, title = Tt}, [NewTest3, NewTest2, NewTest1 | Acc])
     end;
 gen_test3([Line | T], ?GETTING_RESULT, Test, Acc) ->
-    io:format("in gen_test3 (4) ~ts~n", [Line]),
+    io:format("in gen_test3 (5) ~ts~n", [Line]),
     #test{resultsacc = R} = Test,
     gen_test3(T, ?GETTING_RESULT, Test#test{resultsacc = [string:trim(Line, trailing, "\n") | R]}, Acc);
 gen_test3([Line | T], ?GETTING_LAZY, Test, Acc) ->
-    io:format("in gen_test3 (5) ~ts~n", [Line]),
+    io:format("in gen_test3 (6) ~ts~n", [Line]),
     #test{lazyacc = R} = Test,
     gen_test3(T, ?GETTING_RESULT, Test#test{lazyacc = [string:trim(Line, trailing, "\n") | R]}, Acc);
 gen_test3([Line | T], ?GETTING_TEST, Test, Acc) ->
-    io:format("in gen_test3 (6) ~ts~n", [Line]),
+    io:format("in gen_test3 (7) ~ts~n", [Line]),
     #test{codeacc = C} = Test,
     gen_test3(T, ?GETTING_TEST, Test#test{codeacc = [string:trim(Line, trailing, "\n") | C]}, Acc);
 gen_test3(["## " ++ Title | T], ?IN_TEXT, Test, Acc) ->
-    io:format("in gen_test3 (7) Title ~p~n", [Title]),
+    io:format("in gen_test3 (8) Title ~p~n", [Title]),
     NewTitle = normalise(Title),
     gen_test3(T, ?IN_TEXT, Test#test{title = NewTitle}, Acc);
 gen_test3([_H | T], ?IN_TEXT, Test, Acc) ->
-    io:format("in gen_test3 (8) discard ~ts~n", [_H]),
+    io:format("in gen_test3 (9) discard ~ts~n", [_H]),
     gen_test3(T, ?IN_TEXT, Test, Acc).
 
 normalise(Text) ->
