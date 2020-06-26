97034060d3a0f1a272bf09fe0dbc1c64ccef045b
Gordon Guthrie <gordon@hypernumbers.com>
2020-06-26 10:57:41 +0200

    try and add lazy results (debuggin III)

--- src/pometo_docs_to_tests_prv.erl
+++ src/pometo_docs_to_tests_prv.erl
@@ -99,7 +99,7 @@ gen_test2(Filename, Lines, GeneratedTestDir) ->
 %% as then the first failing test you should fix appears at the bottom
 gen_test3([], _, _, Acc) -> lists:flatten(Acc);
 gen_test3(["```" ++ _Rest | T], ?GETTING_TEST, Test, Acc) ->
-    io:format("in gen_test3 (2)~n", []),
+    io:format("in gen_test3 (2) Test is ~p~n", [Test]),
     #test{seq        = N,
           title      = Tt,
           codeacc    = C,
@@ -125,15 +125,15 @@ gen_test3(["```" ++ _Rest | T], ?GETTING_TEST, Test, Acc) ->
         gen_test3(T, ?IN_TEXT, #test{seq = N + 1, title = Tt}, [NewTest3, NewTest2, NewTest1 | Acc])
     end;
 gen_test3([Line | T], ?GETTING_RESULT, Test, Acc) ->
-    io:format("in gen_test3 (3) ~p~n", [Line]),
+    io:format("in gen_test3 (3) ~ts~n", [Line]),
     #test{resultsacc = R} = Test,
     gen_test3(T, ?GETTING_RESULT, Test#test{resultsacc = [string:trim(Line, trailing, "\n") | R]}, Acc);
 gen_test3([Line | T], ?GETTING_LAZY, Test, Acc) ->
-    io:format("in gen_test3 (4) ~p~n", [Line]),
+    io:format("in gen_test3 (4) ~ts~n", [Line]),
     #test{lazyacc = R} = Test,
     gen_test3(T, ?GETTING_RESULT, Test#test{lazyacc = [string:trim(Line, trailing, "\n") | R]}, Acc);
 gen_test3([Line | T], ?GETTING_TEST, Test, Acc) ->
-    io:format("in gen_test3 (5) ~p~n", [Line]),
+    io:format("in gen_test3 (5) ~ts~n", [Line]),
     #test{codeacc = C} = Test,
     gen_test3(T, ?GETTING_TEST, Test#test{codeacc = [string:trim(Line, trailing, "\n") | C]}, Acc);
 gen_test3(["```pometo_results" ++ _Rest | T], ?IN_TEXT, Test, Acc) ->
@@ -150,7 +150,7 @@ gen_test3(["## " ++ Title | T], ?IN_TEXT, Test, Acc) ->
     NewTitle = normalise(Title),
     gen_test3(T, ?IN_TEXT, Test#test{title = NewTitle}, Acc);
 gen_test3([_H | T], ?IN_TEXT, Test, Acc) ->
-    io:format("in gen_test3 (10) discard ~p~n", [_H]),
+    io:format("in gen_test3 (10) discard ~ts~n", [_H]),
     gen_test3(T, ?IN_TEXT, Test, Acc).
 
 normalise(Text) ->
